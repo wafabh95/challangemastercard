63a25c844f847f03d0145371643a8f87a1d61726
wafabh95 <bhammoudawafa@gmail.com>
2021-03-17 12:50:35 +0100

    master card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+
+import mastercard from './facebook-actu-n26-business-metal.jpg'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 import './App.css';
+import { useState } from "react"
 
 function App() {
+  const [number, setNumber] = useState("****************");
+  const [date, setDate] = useState("1503");
+  const [name, setName] = useState("");
+const maxnumber = 16;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div className="App-header" style={{backgroundImage:`url(${mastercard})`,backgroundRepeat: "no-repeat",backgroundSize: "100% 70%"}}>
+      
+       <p className="num">{number}</p>
+       <p>{date.slice(0,2)}/{date.slice(2,4)}</p>
+       <p>{name}</p>
+      
+       
+  <Form>
+    {console.log()}
+    <Form.Control type="text" placeholder="num card" onChange={(e)=>{e.target.value.length <= maxnumber ?setNumber(number.replace(number.slice(0,e.target.value.length),e.target.value)) : alert("The Number of MasterCard must be less then 16 ")}}/>
+    <Form.Control type="text" placeholder="expiration date" onChange={(e)=>{e.target.value.length<=4 ?setDate(e.target.value):alert("mettre sous forme jj/mm ne dépasse pas 4 chiffre")}}/>
+    <Form.Control type="text" placeholder="Enter name"onChange={(e)=>{setName(e.target.value)}} />
+  </Form>
+
+</div>
+    
+    
+    
   );
 }
 
